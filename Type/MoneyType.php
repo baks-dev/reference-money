8ce07070c6b29f51c00576c54e787338383eba67
kingstar <admin@baks.dev>
2023-10-17 00:26:39 +0300

    Baks Development

--- Type/MoneyType.php
+++ Type/MoneyType.php
@@ -29,19 +29,19 @@ use Doctrine\DBAL\Types\BigIntType;
 final class MoneyType extends BigIntType
 {
 	
-	public function convertToDatabaseValue($value, AbstractPlatform $platform) : mixed
+	public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
 	{
 		return $value instanceof Money ? $value->getValue() * 100 : $value * 100;
 	}
 	
 	
-	public function convertToPHPValue($value, AbstractPlatform $platform) : mixed
+	public function convertToPHPValue($value, AbstractPlatform $platform): mixed
 	{
 		return !empty($value) ? new Money($value / 100) : null; //new Money(0);
 	}
 	
 	
-	public function getName() : string
+	public function getName(): string
 	{
 		return Money::TYPE;
 	}
