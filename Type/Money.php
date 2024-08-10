c3a3efb063b6efe697432e81a1b06b854dd10896
kingstar <admin@baks.dev>
2024-08-10 23:54:58 +0300

    Baks Deevlopment

--- Type/Money.php
+++ Type/Money.php
@@ -1,17 +1,17 @@
 <?php
 /*
  *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -86,6 +86,22 @@ final class Money
 
         return $this;
     }
+
+
+    public function percent(int $percent): self
+    {
+        if($percent < 0 || $percent > 100)
+        {
+            throw new InvalidArgumentException('Для расчета процента значение должно быть от 0 до 100');
+        }
+
+        $current = $this->getValue() * 100;
+
+        $discount = ($current / 100 * $percent) / 100;
+
+        return new self($discount);
+    }
+
 
     public function sub(self $money): self
     {
