120be82bba1330f4860c63e6a325a039c3983560
kingstar <admin@baks.dev>
2024-10-23 20:20:00 +0300

    Baks Deevlopment

--- Twig/MoneyExtension.php
+++ Twig/MoneyExtension.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,27 +31,31 @@ use Twig\TwigFunction;
 
 final class MoneyExtension extends AbstractExtension
 {
-	
-	private TranslatorInterface $translator;
-	
-	
-	public function __construct(TranslatorInterface $translator) {
-		$this->translator = $translator;
-	}
-	
-	
-	public function getFunctions() : array
-	{
-		return [
-			new TwigFunction('money', [$this, 'call']),
-		];
-	}
-	
-	
-	public function call(Money|float|null $money, ?string $from = 'RUR', string $to = null)
-	{
-		
-		if(empty($money)) { return null; }
+
+    private TranslatorInterface $translator;
+
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('money', [$this, 'call']),
+        ];
+    }
+
+
+    public function call(Money|float|null $money, ?string $from = 'RUR', ?string $to = null)
+    {
+
+        if(empty($money))
+        {
+            return null;
+        }
 
         if($money instanceof Money)
         {
@@ -61,21 +65,24 @@ final class MoneyExtension extends AbstractExtension
         {
             $money = ($money * 1 / 100);
         }
-		
-		if(empty($money)) {  return null; }
-		
-//		if(empty($to)) {
-//            $to = $from; }
-		
-		if($from !== $to)
-		{
-			/** TODO:Конвертируем валюту */
-		}
-		
-		$fmt = new NumberFormatter($this->translator->getLocale(), NumberFormatter::CURRENCY);
-		$fmt->setAttribute(NumberFormatter::FRACTION_DIGITS, 2);
 
-		return str_replace([',00', '.00'], '', $fmt->formatCurrency($money, $to ?: 'RUR'));
-	}
-	
+        if(empty($money))
+        {
+            return null;
+        }
+
+        //		if(empty($to)) {
+        //            $to = $from; }
+
+        if($from !== $to)
+        {
+            /** TODO:Конвертируем валюту */
+        }
+
+        $fmt = new NumberFormatter($this->translator->getLocale(), NumberFormatter::CURRENCY);
+        $fmt->setAttribute(NumberFormatter::FRACTION_DIGITS, 2);
+
+        return str_replace([',00', '.00'], '', $fmt->formatCurrency($money, $to ?: 'RUR'));
+    }
+
 }
